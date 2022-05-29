0fa2fdba0c1e42c2bd12f73d6921c879bf7be93c
DarkHoptoad <demennikov.ivan@gmail.com>
2022-05-29 00:22:20 +0000

    gh-pages added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,13 +1,23 @@
-function Footer () {
+function Footer() {
+  return (
+    <footer className="page-footer grey darken-3 footer">
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright by{" "}
+          <span className="dark">Dark</span>
+          <span className="frog">Frog</span>
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/DarkHoptoad/DarkFrogShop"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <txt className="link__txt">Repo</txt>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
-    return <footer className="page-footer grey darken-3 footer">
-              <div className="footer-copyright">
-                <div className="container">
-                © {new Date().getFullYear()} Copyright by <span className="dark">Dark</span><span className="frog">Frog</span>
-                <a className="grey-text text-lighten-4 right" href="#!"><txt className='link__txt'>Repo</txt></a>
-                </div>
-              </div>
-            </footer>
-    }
-    
-    export {Footer}
+export { Footer };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,24 @@
-
-function Header () {
-    return <div className="header unselectable">
-        <div className="header__content">
-            <div className="header__logo">DarkFrog Shop</div>
-            <div className="slogan">Best items in <span className="You">Your</span> game!</div>
-            <div className="header__nav">
-                <a className="nav__link" href="!#" >Repo</a>
-            </div>
+function Header() {
+  return (
+    <div className="header unselectable">
+      <div className="header__content">
+        <div className="header__logo">DarkFrog Shop</div>
+        <div className="slogan">
+          Best items in <span className="You">Your</span> game!
         </div>
+        <div className="header__nav">
+          <a
+            className="nav__link"
+            href="https://github.com/DarkHoptoad/DarkFrogShop"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Repo
+          </a>
+        </div>
+      </div>
     </div>
+  );
 }
 
-export {Header}
+export { Header };
